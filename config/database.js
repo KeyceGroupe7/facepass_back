b2dd0a093ebfaee1656938fa116ad8096c5bfb1f
francoisvid <vidalfrancois11@gmail.com>
2022-03-16 23:32:42 +0100

    add delete + fix css + fix add image

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 const credentials = {
-    cluster: "@cluster0.rk61o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    cluster: "@cluster0.rk61o.mongodb.net/test?retryWrites=true&w=majority",
     user: "root",
     password: "root"
 };
